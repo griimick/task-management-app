7e98b12fe3fd25eee0587c672a5d7772b3a1a2c8
Soumik Pradhan <soumikpradhan14@gmail.com>
2022-09-08 20:15:37 +0530

    Update basic CRUD endpoints using app memory as store

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,71 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { CreateTaskDto } from './dto/creat-task.dto';
+import { v4 as uuid } from 'uuid';
+import { GetTasksFilterDto } from './dto/get-tasks-filer.dto';
+
+@Injectable()
+export class TasksService {
+	private tasks: Task[] = [];
+
+	getAllTasks() {
+		return this.tasks;
+	}
+
+	createTask(createTaskDto: CreateTaskDto): Task {
+		const { title, description } = createTaskDto;
+		const task: Task = {
+			id: uuid(),
+			title,
+			description,
+			status: TaskStatus.OPEN,
+		};
+
+		this.tasks.push(task);
+
+		return task;
+	}
+
+	getTasksWithFilter(filterDto: GetTasksFilterDto): Task[] {
+		const { status, search } = filterDto;
+
+		let tasks = this.getAllTasks();
+
+		if (status) {
+			tasks = tasks.filter((task) => task.status === status);
+		}
+
+		if (search) {
+			tasks = tasks.filter((task) => {
+				if (task.title.includes(search) || task.description.includes(search)) {
+					return true;
+				}
+				return false;
+			});
+		}
+
+		return tasks;
+	}
+
+	getTaskById(id: string): Task {
+		const found = this.tasks.find((task) => task.id === id);
+
+		if (!found) {
+			throw new NotFoundException(`Task with ID "${id}" not found`);
+		}
+
+		return found;
+	}
+
+	deleteTaskById(id: string): void {
+		this.getTaskById(id);
+		this.tasks = this.tasks.filter((task) => task.id !== id);
+		return;
+	}
+
+	updateTaskStatus(id: string, status: TaskStatus): Task {
+		const task = this.getTaskById(id);
+		task.status = status;
+		return task;
+	}
+}
